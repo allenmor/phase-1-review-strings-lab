21179d1b793dc2bad601999b9e1d0b846955c7fb
Allen Mordukhaev <allenmor93@gmail.com>
2022-07-18 16:33:01 -0400

    commit tree 1a1d75e7463c84fc4397dafd96705505beb30a39

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+
+// declaring currentuser
+
+const currentUser = 'Allen';
+
+// declaring welcome message
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// declaring excited welcome message
+
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// declaring short greeting
+
+const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+
